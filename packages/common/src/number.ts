c84fdbbbfcfc4372a43c7a1a73f84e7ac5b833a7
Hzyhhh <799309084@qq.com>
2020-10-21 19:59:26 +0800

    feat: number 模块

---
+++ packages/common/src/number.ts
@@ -0,0 +1,48 @@
+/**
+ * 数字相关的操作
+ */
+
+/**
+ * 遍历n次
+ * @example
+ * ```js
+ * times(10, i => i) // [0, 1, 2, ..., 9]
+ * ```
+ *
+ * @param time 次数
+ * @param it 类型转换
+ *
+ * @returns 由第二参数 it 返回值定义数组类型
+ */
+export function times<T>(time: number, it: (index: number) => T): T[] {
+  const arr: T[] = []
+  for (let i = 0; i < time; i++) {
+    arr.push(it(i))
+  }
+  return arr
+}
+
+/**
+ * 获取指定范围整数随机数, 不包括max
+ * @param min
+ * @param max
+ *
+ * @example
+ * ```js
+ * getRandomInt(0, 10) // => 2
+ * ```
+ *
+ */
+export function getRandomInt(min: number, max: number) {
+  const _min = Math.ceil(min)
+  const _max = Math.floor(max)
+  return Math.floor(Math.random() * (_max - _min)) + _min
+}
+
+let uid = 0
+/**
+ * 获取简单的随机数
+ */
+export function getUid() {
+  return (uid = uid + (1 % (Number.MAX_SAFE_INTEGER - 1)))
+}
