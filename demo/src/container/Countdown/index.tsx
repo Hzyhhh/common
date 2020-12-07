adf4a53309e663e429b58f0c03c1bbb60a9aa5c9
Hzyhhh <799309084@qq.com>
2020-12-07 20:31:30 +0800

    feat:[demo]countdown demo implement

---
+++ demo/src/container/Countdown/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Countdown } from '@hzyhhh/components'
+
+// const timeStamp = new Date().valueOf()
+// eslint-disable-next-line
+export default () => {
+  // 在你的组件文件中加入这些代码
+
+  return <Countdown timeStamp={0} />
+  //   return <div >123</div>
+}
---
+++ demo/src/container/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import Countdown from './Countdown'
+
+export const App: FC = () => {
+  return <Countdown></Countdown>
+}
+
+export default App
