ea03774689d0bce1c696387b76f399874beb3bce
Hzyhhh <799309084@qq.com>
2020-12-08 17:51:50 +0800

    feat: [hooks] :sparkles: useTpggle

---
+++ packages/hooks/src/useToggle.ts
@@ -0,0 +1,25 @@
+import { useState, useCallback } from 'react'
+
+/**
+ * 提供开关属性
+ * @param defaultValue
+ */
+export default function useToggle(
+  defaultValue?: boolean,
+  adapter: (
+    defaultValue?: boolean,
+  ) => [
+    boolean | undefined,
+    (cb: (value: boolean | undefined) => boolean) => void,
+    ...any[]
+  ] = useState,
+): [boolean | undefined, () => void] {
+  const [value, setValue] = adapter(defaultValue)
+
+  return [
+    value,
+    useCallback(() => {
+      setValue(v => !v)
+    }, []),
+  ]
+}
