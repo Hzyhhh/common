73ba5fffb7b578f8facf42d0764e5f541bae2d23
Hzyhhh <799309084@qq.com>
2021-01-31 19:50:39 +0800

    feat:  [common] 初步实现二分法 暂未验证

---
+++ packages/common/src/devide.ts
@@ -0,0 +1,22 @@
+
+/**
+ * 二分法 匹配数组中的元素
+ * 排序数组
+ * TODO: 待验证
+ */
+export function devided(sourceArr: string[] | number[], target: string | number) {
+    let start = 0, end = sourceArr.length - 1, middle, element
+    middle = Math.floor((start + end) / 2)
+    element = sourceArr[middle]
+    while (start <= end) {
+        if (target > element) {
+            start = middle
+            continue
+        } else if (target < element) {
+            end = middle
+            continue
+        } else {
+            return element
+        }
+    }
+}
